187b1ffa8930612cb05168b18294cf0bc8fac657
Vawet <2874184050@qq.com>
2025-07-23 15:34:29 +0800

    按需导入

--- src/services/seriesService.js
+++ src/services/seriesService.js
@@ -7,13 +7,12 @@ export const handleSearch=async (nickname) => {
     const store=useAllDataStore();
     try{
         const res = await axios.post(`${API_URL}/getUserWithPlants`,{nickname});
-        console.log(res.data);
-        console.log(res.data.data.plantGroups);
-        console.log(res.data.data.user);
+        // console.log(res.data);
+        // console.log(res.data.data.plantGroups);
+        // console.log(res.data.data.user);
         store.updatePerson(res.data.data.user);
         store.updatePlantsGroup(res.data.data.plantGroups);
-        console.log(store.plantsGroup)
-        
+        // console.log(store.plantsGroup)
     }catch(error){
         // 处理不同类型的错误
         if (error.response) {
